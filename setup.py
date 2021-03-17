6e4a4892c0a8a63d9b5068469ba228949400eb43
Ahmet Yildirir <ahmetyildirir1@gmail.com>
2021-03-17 15:19:17 +0300

    test

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'Testing_p2121',
+    packages = ['.'],
+    version = '0.1.0',
+    description = 'Test',
+    author = 'Ahmet Yıldırır',
+    author_email = 'ahmetyildirir1@gmail.com',
+    url = 'githubURL',
+    keywords = ['TAG1', 'TAG2'],
+    classifiers = [],
+)
